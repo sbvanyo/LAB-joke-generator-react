210b423cddc3210b16709291c1349ed300743a64
Stacey Vanyo <sbvanyo@gmail.com>
2023-07-10 21:55:20 -0500

    butto n text change + text render onclick

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import React, { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [buttonText, setButtonText] = useState('get a joke');
+  const [jokeSetup, setJokeSetup] = useState('');
+  // const [jokeDelivery, setJokeDelivery] = useState('');
+  const joke = getJoke();
+
+  const handleClick = () => {
+    setButtonText('get punchline');
+    console.warn(joke);
+  };
+
+  const handleSetup = () => {
+    setJokeSetup('test setup');
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -10,7 +27,8 @@ function Home() {
       }}
     >
       <h1>Welcome Home!</h1>
-      <button type="button">get a joke</button>
+      <button type="button" onClick={() => { handleClick(); handleSetup(); }}>{buttonText}</button>
+      <h2>{jokeSetup}</h2>
     </div>
   );
 }
